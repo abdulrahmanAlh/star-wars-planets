833d25aca62c0d79b3d7a84a7885dc94f0396f87
abdulrahmanMeta <107802763+abdulrahmanMeta@users.noreply.github.com>
2022-09-18 03:26:20 +0400

    check people's species

--- src/core/groups/planet.ts
+++ src/core/groups/planet.ts
@@ -4,20 +4,19 @@ import { createGroup, PayloadAction, store } from "../stateManagment";
 
 export interface Planet {
   name: string;
-  classification: string;
-  designation: string;
-  average_height: string;
-  average_lifespan: string;
-  eye_colors: string;
-  hair_colors: string;
-  skin_colors: string;
-  language: string;
-  homeworld: string;
-  people: string[];
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
   films: string[];
-  url: string;
   created: string;
   edited: string;
+  url: string;
 }
 
 interface InitState {
@@ -56,11 +55,20 @@ export const FetchPlanets = async () => {
   try {
     const res = await axios.get("https://swapi.dev/api/planets");
     let planets: Planet[] = res.data.results;
-    // planets = planets.filter((planet) => planet.films.length > 0);
-    // let operations = [];
-    // planets.forEach((plant)=>{
+    planets = planets.filter((planet) => planet.films.length > 0);
+    let operations: any = [];
+    for (let index = 0; index < planets.length; index++) {
+      const plant = planets[index];
+      plant.residents.forEach((person) => {
+        operations.push(axios.get(person));
+      });
+      await Promise.all(operations).then((values) => {
+        // TODO Set condtion here
+        console.log(values);
+      });
+      operations = [];
+    }
 
-    // })
     dispatch(setPlanets(planets));
   } catch (error) {
     dispatch(setError("Error on fetch planets.."));
